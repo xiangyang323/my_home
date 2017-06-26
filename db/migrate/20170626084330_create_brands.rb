7be6cce60478baec475c1802ab8be6fb0bc127d6
sang.mx <sang.mx@maisiteng.com>
2017-06-26 20:07:19 +0800

    品牌架构

---
+++ db/migrate/20170626084330_create_brands.rb
@@ -0,0 +1,6 @@
+class CreateBrands < ActiveRecord::Migration[5.0]
+  def change
+    create_table :brands do |t|
+    end
+  end
+end
---
+++ db/migrate/20170626084342_create_brand_categories.rb
@@ -0,0 +1,6 @@
+class CreateBrandCategories < ActiveRecord::Migration[5.0]
+  def change
+    create_table :brand_categories do |t|
+    end
+  end
+end
