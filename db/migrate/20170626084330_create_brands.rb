53c7acf012918958e495014465edbcd2410d03f1
sang.mx <sang.mx@maisiteng.com>
2017-07-12 19:36:03 +0800

    migrate修改

--- db/migrate/20170626084330_create_brands.rb
+++ db/migrate/20170626084330_create_brands.rb
@@ -10,14 +10,14 @@ class CreateBrands < ActiveRecord::Migration[5.0]
       t.integer :tel, null: false, default: 0
       t.integer :tel_code
       t.integer :business_licence, null: false
-      t.text :business_scope, null: false, default: ""
-      t.text :content, null: false, default: ""
+      t.text :business_scope, null: false
+      t.text :content, null: false
       t.integer :check_flag, null: false, default: 0
       t.integer :status
       t.timestamps
     end
 
-    add_index :brands, :user_id, unique: true
-    add_index :brands, :check_flag, unique: true
+    add_index :brands, :user_id
+    add_index :brands, :check_flag
   end
 end
